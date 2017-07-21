2f8d30427cd72611a828594cbb3de7c2ac15d4f2
Mike <ilnitsky@ngs.ru>
2017-07-22 01:17:50 +0700

    L4_t2_B: эксперименты с z-index картинок - в Firefox работает, в Chrome не работает

--- Lesson4_task2_B/scripts/script.js
+++ Lesson4_task2_B/scripts/script.js
@@ -14,7 +14,12 @@
                     var outline = "outline: 3px solid red;";
                 }else{
                     var outline = "outline: 3px solid black;";
-                }              
+                }
+
+                var zIndex = getComputedStyle(event.target.children[0]).zIndex;
+                console.info("z-index = " + zIndex);
+
+                event.target.children[0].style.zIndex = 1e4;
             
                 var start = Date.now();
             
@@ -24,16 +29,17 @@
                     event.target.children[0].style = outline;
 
                     if (timePassed < 1000){
-                        event.target.children[0].style = outline + "transform: scale(" + (timePassed / 1000 + 1) + ");";
+                        event.target.children[0].style = outline + "z-index: 10000; transform: scale(" + (timePassed / 1000 + 1) + ");";
                     } else if (timePassed < 2000){
-                        event.target.children[0].style = outline + "transform: scale(2)"; 
+                        event.target.children[0].style = outline + "z-index: 1000; transform: scale(2)";
                     } else if (timePassed < 3000){
-                        event.target.children[0].style = outline + "transform: scale(" + ((3000 - timePassed) / 1000 + 1) + ");";
+                        event.target.children[0].style = outline + "z-index: 100; transform: scale(" + ((3000 - timePassed) / 1000 + 1) + ");";
                     } else {
-                        event.target.children[0].style="outline: none; transform: scale(1);";
+                        event.target.children[0].style="z-index: 10; outline: none; transform: scale(1);";
                         clearInterval(timer);
                         return;
                     }
+                    console.info("new z-index = " + getComputedStyle(event.target.children[0]).zIndex);
                 }
 
                 var timer = setInterval(animation, 20);
