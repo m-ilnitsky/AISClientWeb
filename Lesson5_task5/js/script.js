9e32f08aa13a9c883ea2d6cddc7a71bc985d234b
Mike <ilnitsky@ngs.ru>
2017-08-10 02:35:31 +0700

    L5_t5: each исправлен с jQuery на Lazy, исправлена сортировка стран по имени

--- Lesson5_task5/js/script.js
+++ Lesson5_task5/js/script.js
@@ -23,7 +23,27 @@
         $(function(){
             $.when($.get('https://restcountries.eu/rest/v2/currency/usd'))
             .then(function(countries){
-                var usdCountries = Lazy(countries).sortBy(countries.name).value();
+                //var usdCountries = Lazy(countries).sortBy(countries.name).value();
+
+                function compareNames(first, second){
+                    var x = Lazy(first.name).toLowerCase().split('').value();
+                    var y = Lazy(second.name).toLowerCase().split('').value();
+
+                    var length = Math.max(x.length, y.length);
+                    
+                    for(var i = 0; i < length; i++){
+                        if(x[i] !== y[i]) {
+                            if(x[i] === undefined) return 1;
+                            else if(y[i] === undefined) return -1;
+                            else if(x[i] > y[i]) return 1;
+                            else return -1;
+                        } 
+                    }
+
+                    return 0;
+                }
+
+                var usdCountries = Lazy(countries).sort( compareNames ).value();
                 var countUSD = Lazy(usdCountries).length();
                 
                 var noAfricaCountries = Lazy(usdCountries).filter(function(item){return !item.region.startsWith('Africa')}).value();
@@ -32,6 +52,7 @@
                 var noAsiaCountries = Lazy(noAfricaCountries).filter(function(item){return !item.region.startsWith('Asia')}).value();
                 var countNoAsia = Lazy(noAsiaCountries).length();
 
+                console.info(countries);
                 console.info(usdCountries);
                 console.info(noAfricaCountries);
                 console.info(noAsiaCountries);
@@ -40,6 +61,19 @@
                 $('h3.no-africa').html('Из них страны вне Африки (' + countNoAfrica + ') :');
                 $('h3.no-asia').html('Из них страны вне Азии (' + countNoAsia + ') :');
 
+                //Lazy(usdCountries).each(function(country){ $('<li>').html(country.name).appendTo($('div.usd ol')) });
+                //Lazy(noAfricaCountries).each(function(country){ $('<li>').html(country.name).appendTo($('div.no-africa ol')) });
+                //Lazy(noAsiaCountries).each(function(country){ $('<li>').html(country.name).appendTo($('div.no-asia ol')) });
+
+                function addLi(element, className){ 
+                    $('<li>').html(element).appendTo($('div.' + className +' ol'));
+                }
+
+                Lazy(usdCountries).each( function(country){ addLi(country.name, 'usd') } );
+                Lazy(noAfricaCountries).each( function(country){ addLi(country.name, 'no-africa') } );
+                Lazy(noAsiaCountries).each( function(country){ addLi(country.name, 'no-asia') } );
+
+                /*
                 $.each(usdCountries, function(index, value){
                     $('<li>').html(value.name).appendTo($('div.usd ol'));
                 });
@@ -51,6 +85,7 @@
                 $.each(noAsiaCountries, function(index, value){
                     $('<li>').html(value.name).appendTo($('div.no-asia ol'));
                 });
+                */
             })
             .fail(function(xhr, textStatus, errorThrown){
             })
