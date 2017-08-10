c49daf0a45e83f2020bb11e5a4fce4fd552f17f9
Mike-Nsk <ilnitsky@ngs.ru>
2017-08-10 20:33:57 +0700

    L6_t3: сделано!

---
+++ Lesson6_task3/main/js/config.js
@@ -0,0 +1,8 @@
+require.config({
+    deps: ['main'],
+    paths: {
+        jquery: '../../node_modules/jquery/dist/jquery',
+        jqueryui: '../../node_modules/jqueryui/jquery-ui',
+        banners: '../../banners/banners'
+    }
+});
